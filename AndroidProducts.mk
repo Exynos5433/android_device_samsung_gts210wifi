9072173958d60edef61ce53b30c764fc8dfc7f8c
ripee <ripee.dev@gmail.com>
2021-01-08 01:12:41 +0100

    gts210wifi: Havoc-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018 The havocOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_gts210wifi.mk
+    $(LOCAL_DIR)/havoc_gts210wifi.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_gts210wifi-user \
-    lineage_gts210wifi-userdebug \
-    lineage_gts210wifi-eng
+    havoc_gts210wifi-user \
+    havoc_gts210wifi-userdebug \
+    havoc_gts210wifi-eng
